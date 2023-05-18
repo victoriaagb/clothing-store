09e072b3067c303f8c522809566156cfb4721da0
Victoria Garcia <vgarcia@asimov.local>
2023-05-18 09:17:37 -0400

    adding category components and styles

---
+++ src/components/category-menu/category-menu.component.jsx
@@ -0,0 +1,14 @@
+import CategoryItem from "../category-item/category-item.component";
+import "./category-menu.styles.scss";
+
+const CategoryMenu = ({ categories }) => {
+  return (
+    <div className="menu-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default CategoryMenu;
